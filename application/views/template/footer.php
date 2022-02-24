ee92bb689ce5400baf4fa590585195149969428c
Raymond-15 <raymondputra15@gmail.com>
2022-02-24 23:25:22 +0800

    konfigurasi template

---
+++ application/views/template/footer.php
@@ -0,0 +1,25 @@
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> beta 1.0
+  </div>
+  <strong>Copyright &copy; 2022 <a href="https://www.instagram.com/raymond_ps_/">RPS</a>.</strong> All rights reserved.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="<?= base_url('assets'); ?>/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= base_url('assets'); ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets'); ?>/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= base_url('assets'); ?>/dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
---
+++ application/views/template/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $judul; ?></title>
+  <!-- icon tab -->
+  <link rel="shortcut icon" href="<?= base_url('assets'); ?>/images/hand-solid.svg" />
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/fontawesome-free/css/all.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="<?= base_url('assets'); ?>/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?= base_url('assets'); ?>/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-footer-fixed layout-navbar-fixed">
+  <!-- Site wrapper -->
+  <div class="wrapper">
